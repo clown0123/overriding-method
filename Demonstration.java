b2fd183c04057540396ee457639c16ba6e74404b
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-07-04 08:29:33 +0700

    Create Demonstration.java

---
+++ Demonstration.java
@@ -0,0 +1,8 @@
+public class Demonstration6 {
+  public static void main(String[] args ){
+      System.out.println("***Method overriding demonstration.***");
+      ChildClass childOb = new ChildClass();
+      childOb.doNotChangeMe();
+      childOb.showMe();
+  }
+}

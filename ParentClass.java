28197fbf1c22d40afce973aa9ae18457a99ff0ed
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-07-04 08:30:07 +0700

    Create ParentClass.java

---
+++ ParentClass.java
@@ -0,0 +1,9 @@
+public class ParentClass  {
+   //overridden method 
+    public void showMe(){
+        System.out.println("Inside ParentClass.showMe()");
+    }
+    public void doNotChangeMe(){
+        System.out.println("Inside ParentClass.doNotChangeMe().");
+    }
+}
